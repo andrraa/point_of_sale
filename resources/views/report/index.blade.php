f06a527647b020ed0d371471a7f2c8bf15ebbc68
andrraa <andraramadanp@outlook.com>
2025-05-27 20:49:51 +0700

    chore: Update

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -5,7 +5,151 @@
 @section('navTitle', 'Daftar Laporan')
 
 @section('content')
-    <div class="bg-white rounded-lg p-8 border border-gray-200">
+    @php
+        $today = \Carbon\Carbon::today()->toDateString();
+    @endphp
 
+    {{-- PENJUALAN --}}
+    <div class="bg-white rounded-lg p-6 border border-gray-200 mb-4">
+        <div class="border-b border-b-gray-200 pb-2 mb-2">
+            <h2 class="text-blue-900 tracking-wide font-medium">Laporan Penjualan</h2>
+        </div>
+
+        <div class="mb-6 grid md:grid-cols-4 gap-8 items-end">
+            <div>
+                <x-form.label :props="[
+                    'for' => 'sale_start_date',
+                    'label' => 'Tanggal Mulai',
+                    'required' => true,
+                ]" />
+                <x-form.input :props="[
+                    'type' => 'date',
+                    'id' => 'sale_start_date',
+                    'name' => 'sale_start_date',
+                    'value' => request('sale_start_date', $today),
+                ]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'sale_end_date',
+                    'label' => 'Tanggal Akhir',
+                    'required' => true,
+                ]" />
+                <x-form.input :props="[
+                    'type' => 'date',
+                    'id' => 'sale_end_date',
+                    'name' => 'sale_end_date',
+                    'value' => request('sale_end_date', $today),
+                ]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'sale_type',
+                    'label' => 'Tipe',
+                    'required' => true,
+                ]" />
+                <x-form.select :props="[
+                    'id' => 'sale_type',
+                    'name' => 'sale_type',
+                    'value' => null,
+                    'class' => 'w-full',
+                ]" :options="[]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'sale_category',
+                    'label' => 'Kategori',
+                    'required' => true,
+                ]" />
+                <x-form.select :props="[
+                    'id' => 'sale_category',
+                    'name' => 'sale_category',
+                    'value' => null,
+                    'class' => 'w-full',
+                ]" :options="[]" />
+            </div>
+
+            <div>
+                <button type="submit"
+                    class="w-full py-[10px] bg-blue-900 text-white tracking-wide font-medium rounded-md transition duration-200 hover:bg-blue-950">
+                    Download
+                </button>
+            </div>
+        </div>
+    </div>
+
+    {{-- PEMBELIAN --}}
+    <div class="bg-white rounded-lg p-6 border border-gray-200 mb-4">
+        <div class="border-b border-b-gray-200 pb-2 mb-2">
+            <h2 class="text-blue-900 tracking-wide font-medium">Laporan Pembelian</h2>
+        </div>
+
+        <div class="mb-6 grid md:grid-cols-4 gap-8 items-end">
+            <div>
+                <x-form.label :props="[
+                    'for' => 'start_date',
+                    'label' => 'Tanggal Mulai',
+                    'required' => true,
+                ]" />
+                <x-form.input :props="[
+                    'type' => 'date',
+                    'id' => 'start_date',
+                    'name' => 'start_date',
+                    'value' => null,
+                ]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'end_date',
+                    'label' => 'Tanggal Akhir',
+                    'required' => true,
+                ]" />
+                <x-form.input :props="[
+                    'type' => 'date',
+                    'id' => 'end_date',
+                    'name' => 'end_date',
+                    'value' => null,
+                ]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'purchase_type',
+                    'label' => 'Tipe',
+                    'required' => true,
+                ]" />
+                <x-form.select :props="[
+                    'id' => 'purchase_type',
+                    'name' => 'purchase_type',
+                    'value' => null,
+                    'class' => 'w-full',
+                ]" :options="[]" />
+            </div>
+
+            <div>
+                <x-form.label :props="[
+                    'for' => 'purchase_category',
+                    'label' => 'Kategori',
+                    'required' => true,
+                ]" />
+                <x-form.select :props="[
+                    'id' => 'purchase_category',
+                    'name' => 'purchase_category',
+                    'value' => null,
+                    'class' => 'w-full',
+                ]" :options="[]" />
+            </div>
+
+            <div>
+                <button type="submit"
+                    class="w-full py-[10px] bg-blue-900 text-white tracking-wide font-medium rounded-md transition duration-200 hover:bg-blue-950">
+                    Download
+                </button>
+            </div>
+        </div>
     </div>
 @endsection
